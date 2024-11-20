4542ced4dd22ebf282438a3358eac79f26efd699
carlos garcia <github_cloudlabsuser_2149@cloudlabsaiuser.com>
2024-11-20 17:29:23 +0000

    funcional

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,96 +7,98 @@ namespace MyMvcApp.Controllers;
 public class UserController : Controller
 {
     public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
+    private static int nextId = 1; // Variable estática para mantener el próximo ID disponible
 
-        // GET: User
-        public ActionResult Index()
+    // GET: User
+    public ActionResult Index()
+    {
+        return View(userlist);
+    }
+
+    // GET: User/Details/5
+    public ActionResult Details(int id)
+    {
+        var user = userlist.FirstOrDefault(u => u.Id == id);
+        if (user == null)
         {
-            return View(userlist);
+            return NotFound();
         }
+        return View(user);
+    }
 
-        // GET: User/Details/5
-        public ActionResult Details(int id)
+    // GET: User/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: User/Create
+    [HttpPost]
+    public ActionResult Create(User user)
+    {
+        if (ModelState.IsValid)
         {
-            var user = userlist.FirstOrDefault(u => u.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // GET: User/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: User/Create
-        [HttpPost]
-        public ActionResult Create(User user)
-        {
-            if (ModelState.IsValid)
-            {
-                userlist.Add(user);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
-
-        // GET: User/Edit/5
-        public ActionResult Edit(int id)
-        {
-            var user = userlist.FirstOrDefault(u => u.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // POST: User/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, User user)
-        {
-            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
-            if (existingUser == null)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                existingUser.Name = user.Name;
-                existingUser.Email = user.Email;
-                // Update other properties as needed
-
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
-
-        // GET: User/Delete/5
-        public ActionResult Delete(int id)
-        {
-            var user = userlist.FirstOrDefault(u => u.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // POST: User/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            var user = userlist.FirstOrDefault(u => u.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            userlist.Remove(user);
+            user.Id = nextId++; // Asigna el próximo ID y luego incrementa el contador
+            userlist.Add(user);
             return RedirectToAction(nameof(Index));
         }
+        return View(user);
+    }
+
+    // GET: User/Edit/5
+    public ActionResult Edit(int id)
+    {
+        var user = userlist.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(user);
+    }
+
+    // POST: User/Edit/5
+    [HttpPost]
+    public ActionResult Edit(int id, User user)
+    {
+        var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+        if (existingUser == null)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            existingUser.Name = user.Name;
+            existingUser.Email = user.Email;
+            // Update other properties as needed
+
+            return RedirectToAction(nameof(Index));
+        }
+        return View(user);
+    }
+
+    // GET: User/Delete/5
+    public ActionResult Delete(int id)
+    {
+        var user = userlist.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(user);
+    }
+
+    // POST: User/Delete/5
+    [HttpPost]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        var user = userlist.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        userlist.Remove(user);
+        return RedirectToAction(nameof(Index));
+    }
 }
--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01f751434578b90b46ac6a982a5e25b6438a06a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30158b03b3427f77fefc6ea119593246f8f7a29d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
