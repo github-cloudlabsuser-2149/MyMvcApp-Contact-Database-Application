17a0b950c474d18b6aeef06013957ca79612cabb
github-cloudlabsuser-2149 <github_cloudlabsuser_2149@cloudlabsaiuser.com>
2025-03-26 17:17:54 +0000

    Desafio 2

--- Models/User.cs
+++ Models/User.cs
@@ -9,8 +9,8 @@ namespace MyMvcApp.Models
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
 
     }
 }
